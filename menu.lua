ee3e0b1a7e472deb94a594feb632a1614f37f2be
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-27 23:31:56 +0200

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -4,6 +4,6 @@ local Window = Library.CreateLib("Kolya Lox", "RJTheme3")
 
 local Tab = Window:NewTab("Speed")
 
-Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
 end)
