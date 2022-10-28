87fbfadcf8eaa1737805f5d3952ca67119f3b21a
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-28 11:17:09 +0200

    Delete menu.lua

--- menu.lua
+++
@@ -1,9 +0,0 @@
-local Window = Library.CreateLib("PixelScript", "RJTheme3")
-
-local Tab = Window:NewTab("Teleport")
-
-local Section = Tab:NewSection("TP to spawn")
-
-Section:NewButton("Teleport", "Spawn", function()
-    game:GetService("Workspace")["RNG_Klop"].HumanoidRootPart.CFrame =game:GetService("Workspace")["The Overworld"].CFrame
-end)
