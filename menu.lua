93a8cbbda513f79f1cb1696ce39499f0a9bf83cc
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-28 11:01:56 +0200

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeleveq/Pixel1/main/menu.lua"))()
 local Window = Library.CreateLib("PixelScript", "RJTheme8")
 
 local Tab = Window:NewTab("Teleport")
