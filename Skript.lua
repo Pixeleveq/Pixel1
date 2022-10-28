e3bf8fb09ac7eaa74c3d237ead3c7c6a236afe0c
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-28 11:15:57 +0200

    Add files via upload

---
+++ Skript.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeleveq/Pixel1/main/menu.lua"))()
+
+local Window = Library.CreateLib("PixelScript", "RJTheme8")
+
+local Tab = Window:NewTab("Teleport")
+
+local Section = Tab:NewSection("TP to spawn")
+
+Section:NewButton("Teleport", "Spawn", function()
+    game:GetService("Workspace")["RNG_Klop"].HumanoidRootPart.CFrame =game:GetService("Workspace")["The Overworld"].CFrame
+end)
