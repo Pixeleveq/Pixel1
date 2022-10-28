8bf8b889f81e1450c07490784b6172afa2b17f2f
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-28 11:18:33 +0200

    Update Skript.lua

--- Skript.lua
+++ Skript.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeleveq/Pixel1/main/menu.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("PixelScript", "RJTheme8")
 
