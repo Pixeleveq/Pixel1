64d21cfbcc0932979283147bdcaf7ede7a700641
Pixeleveq <116846088+Pixeleveq@users.noreply.github.com>
2022-10-28 11:23:59 +0200

    Update Skript.lua

--- Skript.lua
+++ Skript.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("PixelScript", "RJTheme8")
+local Window = Library.CreateLib("PixelScript", "RJTheme3")
 
 local Tab = Window:NewTab("TabName")
